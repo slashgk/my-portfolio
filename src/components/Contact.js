3b39617486bea7ca22fc9173e8fa99837db3f343
gkumar16 <gaurav_kumar1@intuit.com>
2021-11-05 16:10:01 +0530

    cleanup and form submit

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,24 +1,14 @@
 import React from "react";
 
 export default function Contact() {
-    const [name, setName] = React.useState("");
-    const [email, setEmail] = React.useState("");
-    const [message, setMessage] = React.useState("");
-
-    function encode(data) {
-        return Object.keys(data)
-            .map(
-                (key) => encodeURIComponent(key) + "=" + encodeURIComponent(data[key])
-            )
-            .join("&");
-    }
 
     function handleSubmit(e) {
         e.preventDefault();
-        fetch("/", {
+        const data = new FormData(e.target);
+        fetch("https://formspree.io/f/xdoyrwla", {
             method: "POST",
-            headers: { "Content-Type": "application/x-www-form-urlencoded" },
-            body: encode({ "form-name": "contact", name, email, message }),
+            headers: { 'Accept': 'application/json' },
+            body: data,
         })
             .then(() => alert("Message sent!"))
             .catch((error) => alert(error));
@@ -82,7 +72,6 @@ export default function Contact() {
                             id="name"
                             name="name"
                             className="w-full bg-gray-800 rounded border border-gray-700 focus:border-indigo-500 focus:ring-2 focus:ring-indigo-900 text-base outline-none text-gray-100 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"
-                            onChange={(e) => setName(e.target.value)}
                         />
                     </div>
                     <div className="relative mb-4">
@@ -94,7 +83,6 @@ export default function Contact() {
                             id="email"
                             name="email"
                             className="w-full bg-gray-800 rounded border border-gray-700 focus:border-indigo-500 focus:ring-2 focus:ring-indigo-900 text-base outline-none text-gray-100 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"
-                            onChange={(e) => setEmail(e.target.value)}
                         />
                     </div>
                     <div className="relative mb-4">
@@ -107,7 +95,6 @@ export default function Contact() {
                             id="message"
                             name="message"
                             className="w-full bg-gray-800 rounded border border-gray-700 focus:border-indigo-500 focus:ring-2 focus:ring-indigo-900 h-32 text-base outline-none text-gray-100 py-1 px-3 resize-none leading-6 transition-colors duration-200 ease-in-out"
-                            onChange={(e) => setMessage(e.target.value)}
                         />
                     </div>
                     <button
